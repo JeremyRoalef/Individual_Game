552343adce5e3a6bb63030afb59f7967581989c7
JeremyRoalef <roalef944@morrisville.edu>
2025-12-24 16:52:27 -0500

    Add Basic Input System Setup
    This commit adds the beginnings of the input system. Reference the input architecture document for setup.

---
+++ IndividualGame/Assets/ActionMap.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public enum ActionMap
+{
+    Build,
+    Combat,
+    UI,
+    World
+}
---
+++ IndividualGame/Assets/GameState.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameState : MonoBehaviour
+{
+    public static GameState Instance;
+
+    [SerializeField]
+    PlayerInput playerInput;
+
+    [SerializeField]
+    bool toggleMove;
+    bool canMove = true;
+
+
+    static readonly Dictionary<ActionMap, string> ACTION_MAPS = new Dictionary<ActionMap, string>
+    {
+        { ActionMap.World, "World" },
+        { ActionMap.UI, "UI" },
+        { ActionMap.Combat, "Combat" },
+        { ActionMap.Build, "Build" },
+    };
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Warning: Multiple game states found in scene. Ensure only one game state exists in the scene.");
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (toggleMove)
+        {
+            canMove = !canMove;
+            ToggleActionMap(ActionMap.World, canMove);
+            toggleMove = false;
+        }
+    }
+
+    public void ToggleActionMap(ActionMap actionMap, bool enable)
+    {
+        if (!ACTION_MAPS.TryGetValue(actionMap, out string actionMapName))
+        {
+            Debug.LogWarning($"Warning: {actionMap} not found in action map dictionary.");
+            return;
+        }
+
+        InputActionMap map = playerInput.actions.FindActionMap(actionMapName, true);
+
+        if (enable)
+            map.Enable();
+        else
+            map.Disable();
+    }
+}
