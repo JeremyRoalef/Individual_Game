415c81a8a2428e2629aac9076d60935524b58ed4
JeremyRoalef <roalef944@morrisville.edu>
2025-12-24 18:12:14 -0500

    Player Movement Setup || Camera Setup
    This commit introduces the player movement and camera setup. Player movement is going to be physics-based for reasons that will be clear later. The camera is set up in an isometric perspective.
    
    Some issues with physics collisions are already evident, with a tower physics material taking care of many problems. However, it needs to be reminded that all towers must have the tower physics material attached to its collider.

---
+++ IndividualGame/Assets/Player/Scripts/PlayerMove.cs
@@ -0,0 +1,74 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    InputActionReference moveInput;
+
+    [SerializeField]
+    Rigidbody playerRb;
+
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    [SerializeField]
+    float rotationSpeed = 720f;
+
+    Vector2 moveDir = new Vector2();
+
+    private void OnEnable()
+    {
+        moveInput.action.performed += OnMoveInputPerformed;
+        moveInput.action.canceled += OnMoveInputPerformed;
+    }
+
+    private void OnDisable()
+    {
+        moveInput.action.performed -= OnMoveInputPerformed;
+        moveInput.action.canceled -= OnMoveInputPerformed;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //Note: Movement is done via up/down/left/right, corresponding to +z/-z/+x/-x, respectively
+        if (moveDir.sqrMagnitude <= float.Epsilon)
+        {
+            playerRb.linearVelocity = Vector3.zero;
+            return;
+        }
+
+        Vector3 playerVelocity = new Vector3(
+            moveDir.x,
+            playerRb.linearVelocity.y,
+            moveDir.y
+        ) * moveSpeed;
+
+        playerRb.linearVelocity = playerVelocity;
+        RotateTowardsMovement();
+    }
+
+    private void RotateTowardsMovement()
+    {
+        Vector3 lookDir = playerRb.linearVelocity.normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(lookDir);
+        Quaternion newRotation = Quaternion.RotateTowards(
+            playerRb.rotation,
+            targetRotation,
+            rotationSpeed * Time.fixedDeltaTime
+            );
+        playerRb.MoveRotation(newRotation);
+    }
+
+    private void OnMoveInputPerformed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move input performed");
+        moveDir = context.ReadValue<Vector2>();
+    }
+}
